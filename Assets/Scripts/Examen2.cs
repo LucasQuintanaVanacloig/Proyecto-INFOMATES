95ffe5bd3131193ceb359562e50a564294c33d64
Marcos <marcos_castano@ceroca.cat>
2024-12-12 16:14:16 +0100

    Examenes funcionan perfecto

--- Assets/Scripts/Examen2.cs
+++ Assets/Scripts/Examen2.cs
@@ -5,19 +5,19 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Examen : MonoBehaviour
+public class Examen2 : MonoBehaviour
 {
     public GameObject panel;
     public int respuesta;
     public TextMeshProUGUI problemaText;
     public TMP_InputField respuestaUsuario;
-    bool iscorrect = false;
+    bool correct2 = false;
     bool isplayerColliding = false;
     // Start is called before the first frame update
     void Start()
     {
         respuesta = 0;
-        iscorrect = false;
+        correct2 = false;
         panel.SetActive(false);
     }
 
@@ -29,9 +29,9 @@ public class Examen : MonoBehaviour
             Respuesta();
         }
 
-        if (iscorrect)
+        if (correct2)
         {
-         //   panel.SetActive (false);
+          //  panel.SetActive(false);
 
         }
         //Respuesta();
@@ -42,21 +42,28 @@ public class Examen : MonoBehaviour
     public void Interactuar()
     {
         Debug.Log("¡El personaje ha interactuado con el objeto!");
+        // Aquí puedes añadir la lógica de lo que debe suceder al interactuar
+        // Ejemplos:
+        // Activar una animación
+        // Cambiar el color o estado del objeto
+        // Destruir el objeto
+        // Dar un mensaje al jugador, etc.
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         // if(other.gameObject.tag =="Personaje")
-        if (other.CompareTag("Personaje") && !iscorrect)
+        if (other.CompareTag("Personaje") && !correct2)
         {
-           // Interactuar();   // aqui pasa algo de generar y abrir examen
+            // Interactuar();   // aqui pasa algo de generar y abrir examen
             GenerarExamen();
             // Respuesta();
             isplayerColliding = true;
             // abrir panel del folio del examen en si
             panel.SetActive(true);
         }
-        
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -72,15 +79,16 @@ public class Examen : MonoBehaviour
 
     private void Respuesta()
     {
-        if(respuestaUsuario.text == respuesta.ToString())
+        if (respuestaUsuario.text == respuesta.ToString())
         {
-            iscorrect = true;
+            correct2 = true;
             isplayerColliding = false;
             panel.SetActive(false);
             //resolver examen que sume y tal
-        } else
+        }
+        else
         {
-            iscorrect = false;
+            correct2 = false;
             //GenerarExamen();
             // de momento nada, a futuro que se cabree el fantasma
         }
@@ -92,12 +100,12 @@ public class Examen : MonoBehaviour
         int num1 = rnd.Next(0, 10);
         int num2 = rnd.Next(0, 10);
         int operacion = rnd.Next(3);
-        
+
         switch (operacion)
         {
             case 0:
                 respuesta = num1 + num2;
-                problemaText.text = num1 + " + " + num2 +" =";
+                problemaText.text = num1 + " + " + num2 + " =";
                 break;
             case 1:
                 respuesta = num1 - num2;
@@ -108,7 +116,7 @@ public class Examen : MonoBehaviour
                 problemaText.text = num1 + " x " + num2 + " =";
                 break;
         }
-        
+
 
     }
 
